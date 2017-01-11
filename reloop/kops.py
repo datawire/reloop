15ff29ddefa2438a5990ea8e7484c710b3af56e5
Philip Lombardi <plombardi@datawire.io>
2017-01-11 12:19:28 -0500

    remove stuff that doesn't belong because this project was sourced from another project

--- reloop/kops.py
+++
@@ -1,2 +0,0 @@
-def is_cluster():
-    return False
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': [
-            'reloop=reloop.cli:cli'
+            'reloopd=reloop.reloopd:reloopd'
         ],
     },
     version=versioneer.get_version(),
